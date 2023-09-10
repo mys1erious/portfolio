7988bec72bf16e6e641dc18c5c10d51cd8cd59f4
mys1erious <subtletyzeros@gmail.com>
2023-09-10 15:09:04 +0200

    feat: add link to playground in main page

---
+++ src/app/dev/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import World from "@/features/environment/components/World";
+
+
+export default function Home() {
+    return (
+        <main className="w-screen h-screen">
+             <World/>
+         </main>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-
-import World from "@/features/environment/components/World";
+import Link from "next/link";
 
 
 export default function Home() {
     return (
-        <main className="w-screen h-screen">
-            <World/>
+        <main className="flex min-h-screen flex-col items-center p-24 text-rose-300">
+            <h1 className="text-3xl mb-8">Coming Soon</h1>
+            <Link className="font-bold underline" href="/dev/">Playground View</Link>
         </main>
     );
 };
